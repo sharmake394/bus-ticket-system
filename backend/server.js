dd8c9fdf7adcbc7a9d10d919d9dd1520f989e58e
Sharmake <sharmacade300@gmail.com>
2026-02-22 05:06:44 +0300

    increase api timeout

--- backend/server.js
+++ backend/server.js
@@ -16,20 +16,13 @@ const allowedOrigins = [ "https://bus-ticketsystem.netlify.app",
 
 const app = express();
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps / curl)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error("Not allowed by CORS"));
-    },
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: [
+    "https://bus-ticketsystem.netlify.app",
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
